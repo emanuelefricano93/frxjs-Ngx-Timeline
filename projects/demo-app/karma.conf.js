80e1f270f1433545a6a0fc7f4ee789b8a873d8ea
emanuele.fricano <emanuele.fricano@rgigroup.com>
2021-08-20 13:19:50 +0200

    Update karma.conf

--- projects/demo-app/karma.conf.js
+++ projects/demo-app/karma.conf.js
@@ -37,7 +37,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: false,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
     singleRun: true,
     restartOnFileChange: true
   });
