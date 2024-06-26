2c5f28ec4d3e69ad771bb7d0c347f3051ab69cfc
emanuelefricano <emanuelefricano93@gmail.com>
2024-06-26 15:10:11 +0200

    update prepare github pages script

--- scripts/prepare-github-pages.sh
+++ scripts/prepare-github-pages.sh
@@ -3,3 +3,4 @@
 echo "Prepare github pages ..."
 ng build demo-app --configuration production --base-href https://emanuelefricano93.github.io/frxjs-Ngx-Timeline/
 find . -type f -name "main.*.js" | xargs git add
+git add docs
