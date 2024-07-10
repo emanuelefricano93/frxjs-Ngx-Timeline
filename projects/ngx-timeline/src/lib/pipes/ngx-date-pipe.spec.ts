88829c410bdadcec6f4af4a5aa368058c34a65ed
emanuelefricano <emanuelefricano93@gmail.com>
2024-07-10 22:37:56 +0200

    add pipe unit test

---
+++ projects/ngx-timeline/src/lib/pipes/ngx-date-pipe.spec.ts
@@ -0,0 +1,17 @@
+import { NgxDateFormat } from "../models";
+import { NgxDatePipe } from "./ngx-date-pipe";
+
+describe('TitleCasePipe', () => {
+  // This pipe is a pure, stateless function so no need for BeforeEach
+  const pipe = new NgxDatePipe();
+
+  it('transforms from date to string', () => {
+    const date = new Date('2024-07-10');
+    expect(pipe.transform(date, NgxDateFormat.DAY_MONTH_YEAR, 'it')).toBe('10 luglio 2024');
+  });
+
+  it('transforms from date to string', () => {
+    const date = new Date('2024-07-10');
+    expect(pipe.transform(date, NgxDateFormat.DAY_MONTH_YEAR)).toBe('10 July 2024');
+  });
+});
