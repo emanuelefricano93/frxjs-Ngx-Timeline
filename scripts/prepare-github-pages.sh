410b7607d358403b155f715cf87f8c4c0d189bbf
emanuelefricano <emanuelefricano93@gmail.com>
2025-06-27 12:34:22 +0200

    Release 20.0.4

--- scripts/prepare-github-pages.sh
+++ scripts/prepare-github-pages.sh
@@ -3,4 +3,7 @@
 echo "Prepare github pages ..."
 ng build demo-app --configuration production --base-href https://emanuelefricano93.github.io/frxjs-Ngx-Timeline/
 find . -type f -name "main-*.js" | xargs git add
+rm -rf docs && mkdir docs
+cp -r dist/demo-app/browser/. docs;
+find dist/demo-app/ -maxdepth 1 -type f -exec cp {} docs/ \;
 git add docs
